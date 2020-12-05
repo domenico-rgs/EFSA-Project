d7ad8a5fe5293648128a8c7f45814679e29d45c2
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2020-12-05 22:15:33 +0100

    corrected weight ls, warning removed, added some plots, removed old script

--- function/objectiveTest.m
+++
@@ -1,9 +0,0 @@
-function [FPE,AIC,MDL,SQUARED_R,Cp,BIC] = objectiveTest(n, q, SSR, TSS)
-FPE = ((n+q)/(n-q))*SSR;
-AIC = 2*q/n + log(SSR);
-MDL = log(n)*q/n + log(SSR);
-SQUARED_R=1-((SSR/(n-q-1))/(TSS/(n-1)));
-Cp=(1/n)*(SSR+2*q*(SSR/(n-q)));
-BIC=(1/n)*(SSR+log(n)*q*(SSR/(n-q)));
-end
-
